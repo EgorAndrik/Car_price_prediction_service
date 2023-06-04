d79b63bdd2952abacf539991cb536e912e1f3adf
EgorAndrik <egorandreas28@gmail.com>
2023-06-04 19:46:13 +0700

    Admin update

--- Server/carDetails.py
+++ Server/carDetails.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 
 
@@ -7,24 +6,15 @@ class AddDetails:
         self.additionalData = data
 
     def addData(self):
-        df = pd.read_csv('DataFrames/TrainTestDATA_Second.csv').iloc[:, 1:]
-        insertData = [int(self.additionalData['price']), int(self.additionalData['yearOfRegistration']),
+        df = pd.read_csv('DataFrames/TrainTestDATA.csv').iloc[:, 1:]
+        insertData = [int(self.additionalData['price']), self.additionalData['vehicleType'],
+                      int(self.additionalData['yearOfRegistration']), self.additionalData['gearbox'],
                       int(self.additionalData['powerPS']), int(self.additionalData['kilometer']),
-                      int(self.additionalData['brand']), int(self.additionalData['notRepairedDamage'])]
-        vechicleType = {i: int(i == self.additionalData['vehicleType']) for i in
-                        ['vehicleType_bus', 'vehicleType_cabrio', 'vehicleType_coupe',
-                         'vehicleType_limousine', 'vehicleType_others', 'vehicleType_small car',
-                         'vehicleType_station wagon', 'vehicleType_suv']}
-        gearbox = {i: int(i == self.additionalData['gearbox']) for i in ['gearbox_automatic', 'gearbox_manual']}
-        fuelType = {i: int(i == self.additionalData['fuelType']) for i in ['fuelType_cng', 'fuelType_diesel', 'fuelType_electro',
-                                                            'fuelType_hybrid', 'fuelType_lpg', 'fuelType_petrol']}
-        for i in vechicleType:
-            insertData.append(vechicleType[i])
-        for i in gearbox:
-            insertData.append(gearbox[i])
-        for i in fuelType:
-            insertData.append(fuelType[i])
+                      self.additionalData['fuelType'],
+                      self.additionalData['brand'].lower() if len(self.additionalData['brand'].split()) == 1
+                      else '_'.join(map(lambda x: x.lower(), self.additionalData['brand'].split())),
+                      int(self.additionalData['notRepairedDamage'])]
 
         df.loc[len(df.index)] = insertData
-        df.to_csv('DataFrames/TrainTestDATA_Second.csv')
+        df.to_csv('DataFrames/TrainTestDATA.csv')
         return 'Данные введены'
