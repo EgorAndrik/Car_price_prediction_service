22f6c42f4ac9f6cf86b4215ef03cc72ebed2cbbe
EgorAndrik <egorandreas28@gmail.com>
2023-06-08 19:20:10 +0700

    Server update, adding admin login

--- Server/server.py
+++ Server/server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import json
 from model import CarRegressor
 from carDetails import AddDetails
 
@@ -22,6 +23,17 @@ def addCarDetails():
         result = request.form
         AD = AddDetails({i: result[i] for i in result})
         return render_template("adminRes.html", result=AD.addData())
+
+
+@application.route('/LogInAdmin', methods=['POST'])
+def logInAdmin():
+    login = request.form
+    with open('Logins/Logins.json', 'r') as logs:
+        logins = json.load(logs)
+        if login['userName'] in logins:
+            return render_template("AdminIndex.html", result='ok') if login['password'] == logins['AdminData']\
+                else 'ERROR access denied'
+        return 'ERROR access denied'
 
 
 if __name__ == '__main__':
