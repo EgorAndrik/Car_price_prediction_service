4650d60c95c6bc9f844919a515d15e2bdb28a262
EgorAndrik <egorandreas28@gmail.com>
2023-06-13 14:17:27 +0700

    Update Log in and add Registration

--- Server/server.py
+++ Server/server.py
@@ -3,7 +3,6 @@ import json
 from model import CarRegressor
 from carDetails import AddDetails
 
-
 application = Flask(__name__, template_folder='template')
 
 
@@ -24,25 +23,51 @@ def addCarDetails():
         return render_template("adminRes.html", result=AD.addData())
 
 
-@application.route('/LogInAdmin', methods=['POST'])
-def logInAdmin():
+@application.route('/LogIn', methods=['POST'])
+def logIn():
     login = request.form
     with open('Logins/Logins.json', 'r') as logs:
         logins = json.load(logs)
         if login['userName'] in logins:
-            return render_template("AdminIndex.html", result='ok') if login['password'] == logins['AdminData']\
-                else 'ERROR access denied'
+            if login['userName'] == 'AdminData':
+                return render_template(logins['AdminData'][1], result='ok') \
+                    if login['password'] == logins['AdminData'][0] \
+                    else 'ERROR access denied'
+            else:
+                return render_template('UserIndex.html', userName=login['userName']) \
+                    if login['password'] == logins[login['userName']] else 'ERROR access denied'
         return 'ERROR access denied'
 
 
-@application.route('/PredictonPriceAdmin')
+@application.route('/Registration', methods=['POST'])
+def registration():
+    registrationForm = request.form
+    with open('Logins/Logins.json', 'r') as logs:
+        logins = json.load(logs)
+    with open('Logins/Logins.json', 'w') as logs:
+        logins[registrationForm['userName']] = registrationForm['password']
+        json.dump(logins, logs, ensure_ascii=False, indent='\t')
+    return render_template('UserIndex.html', userName=registrationForm['userName'])
+
+
+@application.route('/AdminData/PredictonPriceAdmin')
 def predictonPriceAdmin():
     return render_template('PredictionAdmin.html')
 
 
-@application.route('/AddCarFormAdmin')
+@application.route('/AdminData/AddCarFormAdmin')
 def addCarFormAdmin():
     return render_template('AdminIndex.html')
+
+
+@application.route('/LoginIndex')
+def loginIndex():
+    return render_template('LoginIndex.html')
+
+
+@application.route('/RegistrationIndex')
+def registrationIndex():
+    return render_template('RegistrationIndex.html')
 
 
 if __name__ == '__main__':
