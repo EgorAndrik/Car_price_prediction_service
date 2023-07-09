e50d95244af4e6e79db9e50bedcaa0022c485ddd
EgorAndrik <egorandreas28@gmail.com>
2023-07-09 17:40:49 +0700

    Update user part

--- Server/server.py
+++ Server/server.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template, request
 import json
 from model import CarRegressor
 from carDetails import AddDetails
+from translatBrandCode import BrandCode
 
 
 application = Flask(__name__, template_folder='template')
+codesBrand = BrandCode()
 
 
 @application.route('/')
@@ -17,6 +19,22 @@ def resultPrediction():
     result = request.form
     resultDict = {i: result[i] for i in result}
     predict = CarRegressor().pred(resultDict)
+    return render_template("resultPrediction.html", result=predict)
+
+
+@application.route('/resultPrediction/<userName>', methods=['POST'])
+def resultPredictionUsers(userName: str):
+    result = request.form
+
+    resultDict, userPredData = {i: result[i] for i in result}, [result[i] for i in result][:8]
+    predict = CarRegressor().pred(resultDict)
+    userPredData.append(predict)
+    userPredData[3] = codesBrand.translationCode(userPredData[3])
+    with open('Logins/Logins.json', 'r') as logs:
+        logins = json.load(logs)
+    with open('Logins/Logins.json', 'w') as logs:
+        logins[userName][1].append(userPredData)
+        json.dump(logins, logs, ensure_ascii=False, indent='\t')
     return render_template("resultPrediction.html", result=predict)
 
 
@@ -40,8 +58,11 @@ def logIn():
                     if login['password'] == logins['AdminData'][0] \
                     else 'ERROR access denied'
             else:
-                return render_template('UserIndex.html', userName=login['userName']) \
-                    if login['password'] == logins[login['userName']] \
+                return render_template('UserIndex.html',
+                                       userName=login['userName'],
+                                       urlHistoryPred=f"http://localhost:5000/HistoriPred/{login['userName']}",
+                                       urlPred=f"http://localhost:5000/resultPrediction/{login['userName']}") \
+                    if login['password'] == logins[login['userName']][0] \
                     else 'ERROR access denied'
         return 'ERROR access denied'
 
@@ -53,10 +74,31 @@ def registration():
         logins = json.load(logs)
     if registrationForm['userName'] not in logins:
         with open('Logins/Logins.json', 'w') as logs:
-            logins[registrationForm['userName']] = registrationForm['password']
+            logins[registrationForm['userName']] = [registrationForm['password'], []]
             json.dump(logins, logs, ensure_ascii=False, indent='\t')
-        return render_template('UserIndex.html', userName=registrationForm['userName'])
+        return render_template('UserIndex.html',
+                               userName=registrationForm['userName'],
+                               urlHistoryPred=f"http://localhost:5000/HistoriPred/{registrationForm['userName']}",
+                               urlPred=f"http://localhost:5000/resultPrediction/{registrationForm['userName']}")
     return "ERROR This user is registered on this service"
+
+
+@application.route('/userPage/<userName>')
+def userPage(userName: str):
+    return render_template('UserIndex.html',
+                           userName=userName,
+                           urlHistoryPred=f"http://localhost:5000/HistoriPred/{userName}",
+                           urlPred=f"http://localhost:5000/resultPrediction/{userName}")
+
+
+@application.route('/HistoriPred/<userName>')
+def historyPredCars(userName: str):
+    with open('Logins/Logins.json', 'r') as logs:
+        dataPredUser = json.load(logs)[userName][1]
+    return render_template('historyPredCars.html',
+                           dataPredUser=dataPredUser,
+                           userName=userName,
+                           urlUserPage=f"http://localhost:5000/userPage/{userName}")
 
 
 @application.route('/AdminData/PredictonPriceAdmin')
---
+++ Server/translatBrandCode.py
@@ -0,0 +1,46 @@
+class BrandCode:
+    def __init__(self):
+        self.dataCodes = {
+            "0": 'alfa romeo',
+            "1": 'audi',
+            "2": 'bmw',
+            "3": 'chevrolet',
+            "4": 'chrysler',
+            "5": 'citroen',
+            "6": 'dacia',
+            "7": 'daewoo',
+            "8": 'daihatsu',
+            "9": 'fiat',
+            "10": 'ford',
+            "11": 'honda',
+            "12": 'hyundai',
+            "13": 'jaguar',
+            "14": 'jeep',
+            "15": 'kia',
+            "16": 'lada',
+            "17": 'lancia',
+            "18": 'land rover',
+            "19": 'mazda',
+            "20": 'mercedes benz',
+            "21": 'mini',
+            "22": 'mitsubishi',
+            "23": 'nissan',
+            "24": 'opel',
+            "25": 'peugeot',
+            "26": 'porsche',
+            "27": 'renault',
+            "28": 'rover',
+            "29": 'saad',
+            "30": 'seat',
+            "31": 'skoda',
+            "32": 'smart',
+            "33": 'subaru',
+            "34": 'suzuki',
+            "35": 'toyota',
+            "36": 'trabant',
+            "37": 'volkswagen',
+            "38": 'volvo'
+        }
+
+    def translationCode(self, code: str) -> str:
+        return self.dataCodes[code]
